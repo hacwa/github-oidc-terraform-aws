e0449bc330775651be2bc492bbf30c2f9790aaf5
Stuart <Stuart.kennedy@ans.co.uk>
2023-05-03 15:08:11 +0100

    remove?

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-resource "aws_ssm_parameter" "foo" {
-    name = "foo"
-    type = "String"
-    value = "bar"  
-}
+#resource "aws_ssm_parameter" "foo" {
+#    name = "foo"
+#    type = "String"
+#    value = "bar"  
+#}##
 
-resource "aws_ssm_parameter" "treefiddy" {
-    name = "ooh"
-    type = "String"
-    value = "small"  
-}
+#resource "aws_ssm_parameter" "treefiddy" {
+#    name = "ooh"
+#    type = "String"
+#    value = "small"  
+#}
